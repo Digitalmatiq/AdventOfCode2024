cd263695a3771def20fe11968036a241a795534e
Damian Radinoiu <damian.radinoiu@galaxsys.co>
2024-12-11 13:03:14 +0200

    init commit

---
+++ AdventOfCode2024/Day1/Day1.cs
@@ -0,0 +1,31 @@
+﻿namespace AdventOfCode2024.Day1;
+
+public static class Day1
+{
+   public static async Task<int> GetAnswer()
+   {
+      var lines = await File.ReadAllLinesAsync(@"Day1\day1.txt");
+
+      var pairs = lines
+         .Select(x => x
+            .Split("   ")
+            .Select(int.Parse)
+            .ToList())
+         .ToList();
+
+      var firstList = pairs
+         .Select(x => x[0])
+         .ToList();
+
+      var secondList = pairs
+         .Select(x => x[1])
+         .ToList();
+
+      firstList.Sort();
+      secondList.Sort();
+
+      var differences = firstList.Zip(secondList).Sum(x => Math.Abs(x.First - x.Second));
+
+      return differences;
+   }
+}
---
+++ AdventOfCode2024/Program.cs
@@ -0,0 +1,10 @@
+﻿using AdventOfCode2024.Day1;
+
+internal class Program
+{
+   private static async Task Main(string[] args)
+   {
+      var result = await Day1.GetAnswer();
+      Console.WriteLine(result);
+   }
+}
